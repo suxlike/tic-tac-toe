b408c8bcf14fa64b7a69fba4e18422486c7d2d62
Umut Toker <utoker@gmail.com>
2021-06-25 08:27:14 -0400

    added win logic and restart

--- public/app.js
+++ public/app.js
@@ -2,13 +2,37 @@ const gameModule = (function () {
   /////////CHACHE
   let gameArray = ["", "", "", "", "", "", "", "", ""];
   const gameBoard = document.querySelector(".game-board");
+  const restartbtn = document.querySelector(".reset-button");
+  let gameText = document.querySelector(".game-text").textContent;
+  console.log(gameText);
+  gameText = "asdsa";
   let round = 0;
-  let a, b, c, rest;
+  const winConditions = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
   /////////
-  let buttonClickHandler = function (event) {
+  const buttonClickHandler = function (event) {
     round % 2 === 0 ? playX(event) : playO(event);
     round++;
     winnerCheck();
+  };
+  const restartHandler = function () {
+    gameArray = ["", "", "", "", "", "", "", "", ""];
+    round = 0;
+    document
+      .querySelectorAll(".game-board button")
+      .forEach((button) => (button.textContent = ""));
+    const buttons = document.querySelectorAll(".game-board button");
+    for (let i = 0; i < buttons.length; i++) {
+      buttons[i].disabled = false;
+    }
   };
 
   /////////SET BOARD
@@ -18,29 +42,49 @@ const gameModule = (function () {
     gameBoard.appendChild(button);
     button.addEventListener("click", buttonClickHandler);
   }
+  restartbtn.addEventListener("click", restartHandler);
   ////////////////////////////////////////////////////////////////
   const playX = function (event) {
+    gameText = "turn O";
     let index = event.target.dataset.index;
-    gameArray.splice(index, 1, "X");
-
-    event.target.textContent = "X";
+    if (gameArray[index] === "") {
+      gameArray.splice(index, 1, "X");
+      event.target.textContent = "X";
+    } else return;
   };
   const playO = function (event) {
+    gameText = `turn X`;
     let index = event.target.dataset.index;
-    gameArray.splice(index, 1, "O");
-
-    event.target.textContent = "O";
+    if (gameArray[index] === "") {
+      gameArray.splice(index, 1, "O");
+      event.target.textContent = "O";
+    } else return;
   };
   ////////////////////////////////////////////////////////////////
 
   const winnerCheck = function () {
-    [a, b, c, ...rest] = gameArray;
-    console.log(gameArray);
-    console.log(["X", "X", "X", ...rest]);
-    gameArray == ["X", "X", "X", ...rest]
-      ? console.log("win")
-      : console.log("not yet");
+    for (let i = 0; i < 8; i++) {
+      let win = winConditions[i];
+      let a = gameArray[win[0]];
+      let b = gameArray[win[1]];
+      let c = gameArray[win[2]];
+      if (a === "" || b === "" || c === "") continue;
+      if (a === b && b === c) {
+        gameOver();
+        break;
+      }
+    }
+    if (gameArray.includes("") === false) return draw();
   };
-
+  const gameOver = function () {
+    round % 2 === 0 ? (gameText = "O Won") : (gameText = "X won");
+    const buttons = document.querySelectorAll(".game-board button");
+    for (let i = 0; i < buttons.length; i++) {
+      buttons[i].disabled = true;
+    }
+  };
+  const draw = function () {
+    gameText = "Draw";
+  };
   return {};
 })();
